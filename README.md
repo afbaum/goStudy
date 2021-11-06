6faff5e66826913afb969a0422ec511aec15e7df
Amanda <amanda.baum01@gmail.com>
2021-11-06 11:41:33 -0400

    creating functions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 This is a golang program that stores data for possible research in a MongoDB.
+
+One package was created for this project: github.com/afbaum/mongoGoStudy
+
+Several functions or methods were used and at least one returns a value:
+1. MongoConnection is a function the creates a connection to the mongo database, it returns a value which is a pointer to the mongo client
+2. dataEntry function collects data from the html form, it connects to the database and inserts the data into the database
+3. handleRequests methonds handles the functions requested based on the URL entered.
+
+Three items from the Features List
+1. This program reads data from a Mongo Database and uses that data by reporting it in the frontend.
+2. Data from the MongoDB is visualized in a graph
+3. Data is analyzed and information is displayed about the data 
--- main.go
+++ main.go
@@ -46,7 +46,8 @@ type LeftEar struct {
 	Left8000 string `json:"l8000" bson:"l8000"`
 }
 
-func main() {
+// MongoConnection is a function that opens a connection to a MongoDB and returns the open connection so it can be used for CRUD operations
+func MongoConnection() *mongo.Client {
 	clientOptions := options.Client().ApplyURI("mongodb+srv://afbaum69:mic530gra@cluster0.fbpz2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 
@@ -61,60 +62,84 @@ func main() {
 	}
 
 	fmt.Println("Connected to MongoDB!")
+	return client
+}
 
+// Display the home page for the study
+func homePage(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("homepage.html"))
+	tmpl.Execute(w, nil)
+}
+
+// Display the entry form for the study and collect the data into the database
+func dataEntry(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("temp.html"))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
-			return
-		}
-
-		rightDetails := RightEar{
-			Right250:  r.FormValue("r250"),
-			Right500:  r.FormValue("r500"),
-			Right750:  r.FormValue("r750"),
-			Right1000: r.FormValue("r1000"),
-			Right1500: r.FormValue("r1500"),
-			Right2000: r.FormValue("r2000"),
-			Right3000: r.FormValue("r3000"),
-			Right4000: r.FormValue("r4000"),
-			Right6000: r.FormValue("r6000"),
-			Right8000: r.FormValue("r8000"),
-		}
-
-		leftDetails := LeftEar{
-			Left250:  r.FormValue("l250"),
-			Left500:  r.FormValue("l500"),
-			Left750:  r.FormValue("l750"),
-			Left1000: r.FormValue("l1000"),
-			Left1500: r.FormValue("l1500"),
-			Left2000: r.FormValue("l2000"),
-			Left3000: r.FormValue("l3000"),
-			Left4000: r.FormValue("l4000"),
-			Left6000: r.FormValue("l6000"),
-			Left8000: r.FormValue("l8000"),
-		}
-
-		details := Subject{
-			Gender:   r.FormValue("gender"),
-			Age:      r.FormValue("age"),
-			Ioiha:    r.FormValue("ioiha"),
-			Make:     r.FormValue("make"),
-			RightEar: rightDetails,
-			LeftEar:  leftDetails,
-		}
-
-		collection := client.Database("study").Collection("subject")
-
-		insertResult, err := collection.InsertOne(context.TODO(), details)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("Inserted a Document: ", insertResult.InsertedID)
-
+	if r.Method != http.MethodPost {
 		tmpl.Execute(w, nil)
-	})
+		return
+	}
+
+	rightDetails := RightEar{
+		Right250:  r.FormValue("r250"),
+		Right500:  r.FormValue("r500"),
+		Right750:  r.FormValue("r750"),
+		Right1000: r.FormValue("r1000"),
+		Right1500: r.FormValue("r1500"),
+		Right2000: r.FormValue("r2000"),
+		Right3000: r.FormValue("r3000"),
+		Right4000: r.FormValue("r4000"),
+		Right6000: r.FormValue("r6000"),
+		Right8000: r.FormValue("r8000"),
+	}
+
+	leftDetails := LeftEar{
+		Left250:  r.FormValue("l250"),
+		Left500:  r.FormValue("l500"),
+		Left750:  r.FormValue("l750"),
+		Left1000: r.FormValue("l1000"),
+		Left1500: r.FormValue("l1500"),
+		Left2000: r.FormValue("l2000"),
+		Left3000: r.FormValue("l3000"),
+		Left4000: r.FormValue("l4000"),
+		Left6000: r.FormValue("l6000"),
+		Left8000: r.FormValue("l8000"),
+	}
+
+	details := Subject{
+		Gender:   r.FormValue("gender"),
+		Age:      r.FormValue("age"),
+		Ioiha:    r.FormValue("ioiha"),
+		Make:     r.FormValue("make"),
+		RightEar: rightDetails,
+		LeftEar:  leftDetails,
+	}
+
+	// use function to connect to DB and insert data
+	client := MongoConnection()
+
+	collection := client.Database("study").Collection("subject")
+
+	insertResult, err := collection.InsertOne(context.TODO(), details)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted a Document: ", insertResult.InsertedID)
+
+	tmpl.Execute(w, nil)
+}
+
+// handle the different request function for this study
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/form", dataEntry)
+}
+
+func main() {
+
+	handleRequests()
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 	http.ListenAndServe(":8080", nil)
 }
